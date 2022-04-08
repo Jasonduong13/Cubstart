f24d0feac76dd760443d896769fd7f0b2164a613
Jason Duong <jasonduong@berkeley.edu>
2022-04-07 23:06:50 -0700

    final submission

---
+++ hw7/newsfeedSkeleton/Enum/SettingsView.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsView.swift
+//  newsfeedSkeleton
+//
+//  Created by saerom on 4/7/22.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("Settings Screen")
+    }
+}
+
