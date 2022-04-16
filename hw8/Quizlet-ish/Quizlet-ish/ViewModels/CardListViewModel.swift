0249b3922e87fa6b33cee1603f6769b8f5280585
Jason Duong <jasonduong@berkeley.edu>
2022-04-16 16:56:17 -0700

    hw8

---
+++ hw8/Quizlet-ish/Quizlet-ish/ViewModels/CardListViewModel.swift
@@ -0,0 +1,20 @@
+import Foundation
+import Combine
+
+class CardListViewModel: ObservableObject {
+  @Published var cardRepository = CardRepository()
+
+  func add(_ card: Card) {
+    cardRepository.add(card)
+  }
+    @Published var cardViewModels: [CardViewModel] = []
+    private var cancellables: Set<AnyCancellable> = []
+
+    init() {
+      cardRepository.$cards.map { cards in
+        cards.map(CardViewModel.init)
+      }
+      .assign(to: \.cardViewModels, on: self)
+      .store(in: &cancellables)
+    }
+}
